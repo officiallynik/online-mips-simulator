86e21f565d1a597fde1cf2e844574c5517db6965
Nikhil <nikhilownstheworld@gmail.com>
2020-02-24 19:55:19 +0530

    added sample programs

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Navbar.css';
-import Swal from 'sweetalert2';
 // import CODE from '../code';
 // const fs = require('fs');
 
@@ -11,26 +10,28 @@ class Navbar extends React.Component{
     render(){
         return ( 
             <nav className="navbar navbar-expand-sm bg-light">
-
                         <div className = "upload">
-                            <div className="nav-item"> 
-                                {/* <a className="nav-link" href="#">Upload</a>
-                                 */}
-                                 
+                            <div className="nav-item">
                                     <div className="file btn btn-sm btn-primary">
                                         Upload
                                         <input type="file" name="file" id ="fileInput" accept=".asm" onChange={this.props.setFile.bind(this)} multiple/>
                                     </div>
                             </div>
                             <div className="nav-item"> 
-                                {/* <a className="nav-link" href="#">Upload</a>
-                                 */}
-                                 
                                     <div className="file btn btn-sm btn-warning" onClick={this.props.deleteFile.bind(this)} >
                                         Clear
                                     </div>
-                            </div>
+                            </div>   
                         </div>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Sample Programs
+                            </a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a className="dropdown-item" href="#">Bubble Sort</a>
+                                <a className="dropdown-item" href="#">Sum of first 10 natural numbers</a>
+                            </div>
+                        </li>
 
                         <div className="brand">
                             MIPS SIMULATOR
